91c5c666cd7fe7b1814967f58793e057ec6cce75
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-09-19 23:43:10 +0200

    Refactoring and editing the design

--- Chat page/assets/js/chatPageMain.js
+++ Chat page/assets/js/chatPageMain.js
@@ -1,28 +1,28 @@
-const optionsSection = document.querySelector(".options");
+const infoSection = document.querySelector(".info");
 const chatsSection = document.querySelector(".chats");
 const chatAreaSection = document.querySelector(".chat-area");
 
-function openOptions() {
+function openInfo() {
   if (window.innerWidth > 1199) {
-    optionsSection.classList.remove("close-options");
+    infoSection.classList.remove("close-info");
     chatsSection.classList.remove("expand-chats");
     chatAreaSection.classList.remove("expand-chat-area");
   } else {
-    optionsSection.classList.add("open");
+    infoSection.classList.add("open");
   }
 }
 
-function closeOptions() {
+function closeInfo() {
   if (window.innerWidth > 1199) {
-    optionsSection.classList.add("close-options");
+    infoSection.classList.add("close-info");
     chatsSection.classList.add("expand-chats");
     chatAreaSection.classList.add("expand-chat-area");
   } else {
-    optionsSection.classList.remove("open");
+    infoSection.classList.remove("open");
   }
 }
 
-function openChats() {
+function toggleChats() {
   if (chatsSection.classList.contains("open"))
     chatsSection.classList.remove("open");
   else chatsSection.classList.add("open");
