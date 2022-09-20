420d8932f3565de4234d2cb2500336bc4c41ccc0
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-09-20 14:49:34 +0200

    Refactoring and editing the design and functionalities

--- Chat page/assets/js/chatPageMain.js
+++ Chat page/assets/js/chatPageMain.js
@@ -1,29 +1,17 @@
-const infoSection = document.querySelector(".info");
-const chatsSection = document.querySelector(".chats");
-const chatAreaSection = document.querySelector(".chat-area");
-
-function openInfo() {
-  if (window.innerWidth > 1199) {
-    infoSection.classList.remove("close-info");
-    chatsSection.classList.remove("expand-chats");
-    chatAreaSection.classList.remove("expand-chat-area");
-  } else {
-    infoSection.classList.add("open");
-  }
+function toggleSection(sectionName) {
+  const section = document.querySelector(`.${sectionName}`);
+  if (section.classList.contains("open")) section.classList.remove("open");
+  else section.classList.add("open");
 }
 
-function closeInfo() {
-  if (window.innerWidth > 1199) {
-    infoSection.classList.add("close-info");
-    chatsSection.classList.add("expand-chats");
-    chatAreaSection.classList.add("expand-chat-area");
-  } else {
-    infoSection.classList.remove("open");
-  }
-}
+const infoItemsSection = document.querySelector(".info-items");
 
-function toggleChats() {
-  if (chatsSection.classList.contains("open"))
-    chatsSection.classList.remove("open");
-  else chatsSection.classList.add("open");
+function toggleInfoItem(item) {
+  if (infoItemsSection.style.display != "none") {
+    infoItemsSection.style.display = "none";
+    document.querySelector(`.${item}`).style.display = "block";
+  } else {
+    infoItemsSection.style.display = "block";
+    document.querySelector(`.${item}`).style.display = "none";
+  }
 }
