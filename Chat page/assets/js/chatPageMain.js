95fe99b5ad32ee7645b30562f99226149d593aa7
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-09-26 20:22:03 +0200

    Finishing message area

--- Chat page/assets/js/chatPageMain.js
+++ Chat page/assets/js/chatPageMain.js
@@ -5,30 +5,102 @@ function toggleSection(sectionName) {
 }
 
 const infoItemsSection = document.querySelector(".info-items");
+const infoDetailsSection = document.querySelector(".info-details");
 
 function toggleInfoItem(item) {
   if (infoItemsSection.style.display != "none") {
     infoItemsSection.style.display = "none";
+    infoDetailsSection.style.display = "block";
     document.querySelector(`.${item}`).style.display = "block";
   } else {
     infoItemsSection.style.display = "block";
+    infoDetailsSection.style.display = "none";
     document.querySelector(`.${item}`).style.display = "none";
   }
 }
 
-function formatText(type) {
-  const sel = window.getSelection();
-  const text = sel.toString();
-  let range = sel.getRangeAt(0);
+let totalMessages = [],
+  iframe,
+  iWindow,
+  iDocument,
+  iParagraph;
 
-  if (sel.rangeCount) {
-    let element = document.createElement(type);
-    if (type === "span") element.style.fontWeight = 400;
-    element.innerHTML = text;
+function initializeData() {
+  iframe = document.querySelector("#mytextarea_ifr");
+  iWindow = iframe.contentWindow;
+  iDocument = iWindow.document;
+}
 
-    range.deleteContents();
-    range.insertNode(element);
+function sendMessage() {
+  iParagraph = iDocument.getElementsByTagName("p")[0];
+  console.log(iParagraph);
+
+  if (iParagraph.textContent === "") {
+    Toastify({
+      text: "Message can't be empty",
+      duration: 2000,
+      destination: "https://github.com/apvarun/toastify-js",
+      newWindow: true,
+      close: true,
+      gravity: "top",
+      position: "center",
+      stopOnFocus: true,
+      style: {
+        background: "#b91d1d",
+      },
+    }).showToast();
+  } else {
+    const messages =
+      iDocument.querySelectorAll("p") || iDocument.querySelectorAll("div");
+
+    console.log(messages);
+    createMessage("sender", "Mohamed Emad", "9:00 PM", messages);
   }
 }
 
-// console.log(document.querySelector(".mce-content-body p"));
+function createMessage(type, name, date, messages) {
+  const conversation = document.querySelector(".conversation");
+
+  const messageSender = document.createElement("div");
+  messageSender.classList.add(type);
+
+  const information = document.createElement("div");
+  information.classList.add("information");
+
+  const image = document.createElement("img");
+  image.setAttribute("src", "assets/img/testimonial-2-nurhansamy.jpg");
+  image.setAttribute("alt", `${type} image`);
+
+  const title = document.createElement("div");
+  title.classList.add("title");
+
+  const titleName = document.createElement("h3");
+  titleName.textContent = name;
+
+  const titleDate = document.createElement("span");
+  titleDate.classList.add("date");
+  titleDate.textContent = date;
+
+  const messageContainer = document.createElement("div");
+  messageContainer.classList.add("msgContainer");
+
+  conversation.appendChild(messageSender);
+
+  title.appendChild(titleName);
+  title.appendChild(titleDate);
+
+  information.appendChild(image);
+  information.appendChild(title);
+
+  messageSender.appendChild(information);
+  messageSender.appendChild(messageContainer);
+
+  information.appendChild(title);
+
+  for (message of messages) {
+    messageContainer.appendChild(message);
+    totalMessages.push(message.outerHTML);
+  }
+
+  console.log(totalMessages);
+}
